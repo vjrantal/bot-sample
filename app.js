e80faefded1c9da9a8b0d69e36497bb221b54709
Ville Rantala <ville.rantala@microsoft.com>
2016-10-20 17:39:17 +0300

    Compatibility with Azure Functions

--- app.js
+++ app.js
@@ -29,8 +29,47 @@ bot.dialog('/firstRun', [
   }
 ]);
 
-var server = restify.createServer();
-server.post('/api/messages', connector.listen());
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url);
-});
+var listener = connector.listen();
+
+if (process.env.FUNCTIONS_EXTENSION_VERSION) {
+  // If we are in the Azure Functions runtime,
+  // export the function that handles requests.
+  module.exports = function (context, req) {
+    var _status = null;
+    var _body = null;
+    var _respond = function (status, body) {
+      context.res = {
+        status: status || 200,
+        body: body || ''
+      };
+      context.done();
+    };
+    // The response object created here creates a
+    // compatility layer between respose object the bot
+    // expects and the reponse object used in Azure Functions.
+    var res = {
+      send: function (status, body) {
+        _respond(status, body);
+      },
+      status: function (status) {
+        _status = status;
+      },
+      write: function (body) {
+        _body = body;
+      },
+      end: function () {
+        _respond(_status, _body);
+      }
+    };
+    // When request comes in, pass it to bot's listener function.
+    listener(req, res);
+  };
+} else {
+  // When run outside of Azure functions, create a restify
+  // server and pass bot's listener as the handler.
+  var server = restify.createServer();
+  server.post('/api/messages', listener);
+  server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+  });
+}
