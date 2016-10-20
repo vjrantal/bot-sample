b303d2970981b5ac63fccb8e44d5af97fb4b309f
Ville Rantala <ville.rantala@microsoft.com>
2016-10-20 16:50:03 +0300

    Cosmetic change

--- app.js
+++ app.js
@@ -3,18 +3,12 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url);
-});
-
 var connector = new builder.ChatConnector({
   appId: process.env.CHAT_CONNECTOR_APP_ID,
   appPassword: process.env.CHAT_CONNECTOR_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector, { persistConversationData: true });
-server.post('/api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
   session.send('%s, I heard: %s', session.userData.name, session.message.text);
@@ -34,3 +28,9 @@ bot.dialog('/firstRun', [
     session.endDialog('Hi %s, say something to me and I will echo it back.', session.userData.name);
   }
 ]);
+
+var server = restify.createServer();
+server.post('/api/messages', connector.listen());
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
+});
