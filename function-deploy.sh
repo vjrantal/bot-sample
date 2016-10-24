6d973d481fac35e6137dd70e9546d2b1ae0e6911
Ville Rantala <ville.rantala@microsoft.com>
2016-10-24 12:50:26 +0300

    Continuous delivery to Azure Functions

---
+++ function-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+set -o pipefail
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_EVENT_TYPE" = "push" ]
+then
+  git config --global user.name "Travis CI"
+  git config --global user.email "noreply@travis-ci.org"
+  cd /tmp
+  # We redirect any output to /dev/null to hide
+  # any sensitive credential data that might otherwise be exposed.
+  git clone --quiet "https://${GIT_USER}:${GIT_PASSWORD}@${GIT_TARGET}" functions > /dev/null 2>&1
+  cd functions
+  git submodule init
+  git submodule update
+  git submodule foreach git pull origin master
+  git commit -a -m "Deployment commit"
+  git push --quiet origin master > /dev/null 2>&1
+fi
