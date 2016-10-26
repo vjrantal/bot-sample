ecc15767491b4e5af457b0f03dd72f42ac3715fd
Ville Rantala <ville.rantala@microsoft.com>
2016-10-26 14:06:29 +0300

    Refactor command handler to a separate package

--- test.js
+++ test.js
@@ -3,50 +3,50 @@
 var test = require('tape');
 var Promise = require('bluebird');
 
-var Program = require('./Program.js');
+var Command = require('command-handler');
 
 test('bot greets with name only at first run', function (t) {
-  var botProgram = new Program('node', ['index.js']);
+  var botCommand = new Command('node', ['index.js']);
 
-  var emulatorProgram = new Program('mono', [process.env.BOT_FRAMEWORK_EMULATOR_PATH]);
+  var emulatorCommand = new Command('mono', [process.env.BOT_FRAMEWORK_EMULATOR_PATH]);
 
   // Ignore the title lines the emulator output
-  emulatorProgram.setIgnores(/^(User1|Bot1) said:$/);
+  emulatorCommand.setIgnores(/^(User1|Bot1) said:$/);
   // Ignore the input, because the emulator echoes back what
   // the user says
-  emulatorProgram.setIgnoreInput(true);
+  emulatorCommand.setIgnoreInput(true);
 
   // First wait for the bot and the emulator to output the
   // message from which we know they are ready to start testing
   Promise.all([
-    emulatorProgram.waitUntil(/^Send message to bot:$/),
-    botProgram.waitUntil(/^restify listening to.*/)
+    emulatorCommand.waitUntil(/^Send message to bot:$/),
+    botCommand.waitUntil(/^restify listening to.*/)
   ])
     .then(function () {
       // An example initial message from the user
-      return emulatorProgram.input('Hi');
+      return emulatorCommand.input('Hi');
     })
     .then(function (output) {
       // We expect the bot to ask the name first
       t.equal(output, 'Hello... What is your name?');
       // Send the name of this user
-      return emulatorProgram.input('Tester');
+      return emulatorCommand.input('Tester');
     })
     .then(function (output) {
       // The bot should greet with the name we set earlier
       t.equal(output, 'Hi Tester, say something to me and I will echo it back.');
       // Say something to the bot
-      return emulatorProgram.input('Something');
+      return emulatorCommand.input('Something');
     })
     .then(function (output) {
       // The bot should not send the greeting anymore, but instead,
       // the message sent should be echoed back
       t.equal(output, 'Tester, I heard: Something');
 
-      // Wait for both programs to close and then end the testing
+      // Wait for both Commands to close and then end the testing
       return Promise.all([
-        emulatorProgram.close(),
-        botProgram.close()
+        emulatorCommand.close(),
+        botCommand.close()
       ]).then(function () {
         t.end();
       });
