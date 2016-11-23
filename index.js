4d0d28f87abd9935025945e47861c45319001858
Ville Rantala <ville.rantala@microsoft.com>
2016-11-23 09:54:42 +0200

    Add LUIS integration

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ var connector = new builder.ChatConnector({
   appPassword: process.env.CHAT_CONNECTOR_APP_PASSWORD
 });
 
+var LUIS_RECOGNIZER_URL = process.env.LUIS_RECOGNIZER_URL;
+
+var recognizers = [];
+if (LUIS_RECOGNIZER_URL) {
+  recognizers = [new builder.LuisRecognizer(process.env.LUIS_RECOGNIZER_URL)];
+}
+var intents = new builder.IntentDialog({
+  recognizers: recognizers
+});
+
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', [
+bot.dialog('/', intents);
+
+intents.onDefault([
   function (session, args, next) {
     if (!session.userData.name) {
       session.beginDialog('/promptName');
@@ -18,12 +30,31 @@ bot.dialog('/', [
     }
   },
   function (session, results) {
-    if (results.name) {
-      session.userData.name = results.name;
+    if (results.response) {
+      session.userData.name = results.response;
       session.endDialog('Hi %s, say something to me and I will echo it back.', session.userData.name);
     } else {
       session.send('%s, I heard: %s', session.userData.name, session.message.text);
       session.endDialog('Say something else...');
+    }
+  }
+]);
+
+intents.matches('ChangeName', [
+  function (session, args, next) {
+    var name = builder.EntityRecognizer.findEntity(args.entities, 'Name');
+    if (!name) {
+      session.beginDialog('/promptName');
+    } else {
+      next({
+        response: name.entity
+      });
+    }
+  },
+  function (session, results) {
+    if (results.response) {
+      session.userData.name = results.response;
+      session.send('Name changed to %s', results.response);
     }
   }
 ]);
@@ -34,7 +65,7 @@ bot.dialog('/promptName', [
   },
   function (session, results) {
     session.endDialogWithResult({
-      name: results.response
+      response: results.response
     });
   }
 ]);
